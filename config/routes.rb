13d2780fbecbdddb9831fdc47daee1df18d33856
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-07-09 04:47:45 +0000

    Created posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
+  resources :posts
+  
+  get 'about' => 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
