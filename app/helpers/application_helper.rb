302de4053b6bfa20db786ed6b039e6e876984f62
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-07-14 06:15:19 +0000

    Different handmade solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
-  def form_group_tag(errors, &block)
+  def form_group_tag(errors) #, &block)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+      # content_tag :div, capture(&block), class: 'form-group has-error'
+      'form-group has-error'
     else
-      content_tag :div, capture(&block), class: 'form-group'
+      # content_tag :div, capture(&block), class: 'form-group'
+      'form-group'
     end
   end
 end
+
