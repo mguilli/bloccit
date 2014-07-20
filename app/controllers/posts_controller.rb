5a2a2e0b51535e5fed1573c8f0bd6438d21aa915
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-07-20 22:12:12 +0000

    Scoped public and private topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
     @comment = Comment.new
     @comments = @post.comments
     authorize @post
+    authorize @topic
   end
 
   def new
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  default_scope {order('created_at DESC')}
+  default_scope { order('created_at DESC') }
+  scope :visible_to, -> (user){ user ? all : public}
+
+  scope :public, -> {where(public: true)}
+  scope :private, -> {where(public: false)}
 end
